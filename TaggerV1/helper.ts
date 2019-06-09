6c5cfacb7051eb0207396ce1f762bf7465168ebd
Dmitry Serbin <dmitry.serbin@gmail.com>
2019-06-09 14:45:34 +1200

    Add debug support

--- TaggerV1/helper.ts
+++ TaggerV1/helper.ts
@@ -1,7 +1,11 @@
+import Debug from "debug";
+
 import * as ba from "azure-devops-node-api/BuildApi";
 import * as bi from "azure-devops-node-api/interfaces/BuildInterfaces";
 
 import { IHelper } from "./interfaces";
+
+const logger = Debug("artifacts-tagger:Helper");
 
 export class Helper implements IHelper {
 
@@ -15,7 +19,9 @@ export class Helper implements IHelper {
 
     public async getBuild(projectName: string, buildId: number): Promise<bi.Build> {
 
-        const result: bi.Build = await this.buildApi.getBuild(buildId, projectName);
+        const verbose = logger.extend("getBuild");
+
+        const result: bi.Build = await this.buildApi.getBuild(projectName, buildId);
 
         if (!result) {
 
@@ -23,19 +29,27 @@ export class Helper implements IHelper {
 
         }
 
+        verbose(result);
+
         return result;
 
     }
 
     public async getBuildTags(projectName: string, buildId: number): Promise<string[]> {
 
+        const verbose = logger.extend("getBuildTags");
+
         const result: string[] = await this.buildApi.getBuildTags(projectName, buildId);
+
+        verbose(result);
 
         return result;
 
     }
 
     public async getDefinitionBuilds(projectName: string, definitionId: number, tagName: string): Promise<bi.Build[]> {
+
+        const verbose = logger.extend("getDefinitionBuilds");
 
         const result: bi.Build[] = await this.buildApi.getBuilds(
             projectName,
@@ -50,23 +64,33 @@ export class Helper implements IHelper {
             undefined,
             [ tagName ]);
 
+        verbose(result);
+
         return result;
 
     }
 
     public async addBuildTag(projectName: string, build: bi.Build, tagName: string): Promise<void> {
 
+        const verbose = logger.extend("addBuildTag");
+
         console.log(`Adding ${build.buildNumber!} build ${tagName} tag`);
 
         const result = await this.buildApi.addBuildTag(projectName, build.id!, tagName);
+
+        verbose(result);
 
     }
 
     public async deleteBuildTag(projectName: string, build: bi.Build, tagName: string): Promise<void> {
 
+        const verbose = logger.extend("deleteBuildTag");
+
         console.log(`Removing ${build.buildNumber} build ${tagName} tag`);
 
         const result = await this.buildApi.deleteBuildTag(projectName, build.id!, tagName);
+
+        verbose(result);
 
     }
 
