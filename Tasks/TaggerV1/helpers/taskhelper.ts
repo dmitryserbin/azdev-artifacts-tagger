d7bebd4d8a91dd4896507f16f6f3a1a91f060d3d
EinmalIM <sven.carstensen@gmail.com>
2022-04-26 12:35:16 +0200

    Update taskhelper.ts
    Only remove ".type" from the end of an artifact name, not somewhere in the artifact name

--- Tasks/TaggerV1/helpers/taskhelper.ts
+++ Tasks/TaggerV1/helpers/taskhelper.ts
@@ -119,7 +119,7 @@ export class TaskHelper implements ITaskHelper {
 
             const releaseArtifacts: string[] | undefined = artifactVariables.filter(
                 (i) => i.name.match("release.artifacts.*.type") && i.value === "Build")
-                    .map((i) => i.name.replace(".type", ""));
+                    .map((i) => i.name.substring(0, i.name.length - 5));
 
             for (const artifact of releaseArtifacts) {
 
